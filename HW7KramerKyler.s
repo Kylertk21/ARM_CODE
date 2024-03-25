d699fe4db61cfb2790e7e194e3fabf9a97f6bd87
Kyler Kramer <141585974+Kylertk21@users.noreply.github.com>
2024-03-25 14:36:53 -0600

    Update HW7KramerKyler.s

--- HW7KramerKyler.s
+++ HW7KramerKyler.s
@@ -1,9 +1,9 @@
-* ARM Program Framework
-* by Dr. Weiying Zhu
+* HW7 - Hamming Code
+* by Kyler Kramer
 MAX_LEN EQU 100
 	
 	AREA	HW7, CODE, READONLY
-	EXPORT	main		;required by the startup code
+	EXPORT	main	
 	ENTRY
 
 main	
@@ -14,7 +14,7 @@ Start
 	LDRB R4, [R1, R2]
 	CMP R4, #0
 	BEQ IsBitZero
-	CMP R4, 1
+	CMP R4, #1
 	BNE DoneErrDet
 	EOR R3, R3, R2
 IsBitZero
@@ -37,25 +37,33 @@ DoneErrCorr
 	LDR R5, =SrcWord
 	MOV R6, #1
 Loop
-	LDRB [R1, R2]
+	LDRB R4, [R1, R2] 
 	CMP R4, #0
 	BEQ DoneSrcWord
 	CMP R2, R6
 	BLO ADataBit
-	LSL R6, R6, #1 ; DOUBLE CHECK
+	LSL R6, R6, #1 
 	B Hop2
 ADataBit
 	STRB R4, [R5], #1
+Hop2
 	ADD R2, #1 
 DoneSrcWord
 	STRB R4, [R5] 
 	SVC #0x11
 	
 	AREA	HW7Data, DATA, READWRITE
-		
+HCode 		DCB "111111000001101", 0
+SrcWord 	SPACE MAX_LEN
+	
+adrHCode 	DCD HCode
+adrSrcWord 	DCD SrcWord
+	
+	EXPORT adrHCode
+	EXPORT adrSrcWord
 	
 	
 	ALIGN
 
-	END		; MUST be AFTER all CODE/DATA areas
+	END		
 
